5dc882524731f0241ad907e27ab58d5fc91d4137
Joe Tustin <j.tustin@gmail.com>
2019-03-13 23:06:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
   string_array = Array.new
   string_array << string
-  string_array.each {|word| string_array << word}
+  string_array.each {|word| string_array << word.reverse}
   puts string_array
 end
 
